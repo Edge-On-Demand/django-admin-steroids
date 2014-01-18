2a3bc4f72bf7579964f30de5d42feab0ec7be8f8
chris <chris@coronis>
2014-01-17 22:02:48 -0500

    Added decorator and admin queryset optimization.

---
+++ admin_steroids/decorators.py
@@ -0,0 +1,47 @@
+import time
+import math
+
+def retry_exceptionless(tries=3, delay=3, backoff=1, exception_cb=None, commit=True):
+    '''
+    Retries a function or method until it runs without throwing an exception.
+
+    delay sets the initial delay in seconds, and backoff sets the factor by
+    which the delay should lengthen after each failure. backoff must be greater
+    than 1, or else it isn't really a backoff. tries must be at least 0, and
+    delay greater than 0.
+    
+    Based on https://wiki.python.org/moin/PythonDecoratorLibrary#Retry
+    '''
+    if commit:
+        from django.db import transaction
+
+    if backoff < 1:
+        raise ValueError("backoff is %s but must be greater than 1" % (backoff,))
+
+    tries = math.floor(tries)
+    if tries < 0:
+        raise ValueError("tries is %s but must be 0 or greater" % (tries,))
+
+    if delay <= 0:
+        raise ValueError("delay is %s but must be greater than 0" % (delay,))
+
+    def deco_retry(f):
+        def f_retry(*args, **kwargs):
+            mtries, mdelay = tries, delay # make mutable
+            for retry in xrange(mtries):
+                try:
+                    rv = f(*args, **kwargs)
+                    if commit:
+                        transaction.commit()
+                    return rv
+                except Exception, e:
+                    if retry+1 == mtries:
+                        raise
+                    if exception_cb:
+                        exception_cb(e)
+                    # Wait.
+                    time.sleep(mdelay)
+                    # Make future wait longer.
+                    mdelay *= backoff
+        return f_retry # true decorator -> decorated function
+    return deco_retry # @retry(arg[, ...]) -> true decorator
---
+++ admin_steroids/queryset.py
@@ -0,0 +1,67 @@
+
+from django.db import connections
+from django.db.models.query import QuerySet
+
+class ApproxCountQuerySet(QuerySet):
+    """
+    Counting all rows is very expensive on large Innodb tables. This
+    is a replacement for QuerySet that returns an approximation if count()
+    is called with no additional constraints. In all other cases it should
+    behave exactly as QuerySet.
+
+    Only works with MySQL. Behaves normally for all other engines.
+    
+    You'd use it by cloning your queryset, substituting this class
+    
+    e.g. to apply to a ModelAdmin, you'd override queryset() like:
+    
+        def queryset(self, *args, **kwargs):
+            qs = super(MyModelAdmin, self).queryset(*args, **kwargs)
+            qs = qs._clone(klass=ApproxCountQuerySet)
+            return qs
+    
+    Based on code from answer http://stackoverflow.com/a/10446271/247542.
+    """
+
+    def count(self):
+        # Code from django/db/models/query.py
+
+        if self._result_cache is not None and not self._iter:
+            return len(self._result_cache)
+
+        db_backend_name = connections[self.db].client.executable_name.lower()
+#        print 'db_backend_name:',db_backend_name
+        is_postgres = 'psql' in db_backend_name or 'postgres' in db_backend_name or 'postgis' in db_backend_name
+        is_mysql = 'mysql' in db_backend_name
+#        print 'is_postgres:',is_postgres
+
+        query = self.query
+        if (is_postgres or is_mysql) and (
+            not query.where and
+            query.high_mark is None and
+            query.low_mark == 0 and
+            not query.select and
+            not query.group_by and
+            not query.having and
+            not query.distinct):
+            if is_postgres:
+                # Read table count approximation from PostgreSQL's pg_class.
+                cursor = connections[self.db].cursor()
+                # Note, there's a bug in the default execute() that
+                # misquotes arguments by using double-quotes when PG only
+                # uses single-quotes.
+                sql = "SELECT reltuples::int FROM pg_class WHERE oid = '%s'::regclass;" % (self.model._meta.db_table,)
+                cursor.execute(sql)
+                results = cursor.fetchall()
+                return results[0][0]
+            elif is_mysql:
+                # Read table count approximation from MySQL's "SHOW TABLE".
+                cursor = connections[self.db].cursor()
+                cursor.execute("SHOW TABLE STATUS LIKE %s",
+                        (self.model._meta.db_table,))
+                return cursor.fetchall()[0][4]
+            else:
+                raise NotImplementedError
+        else:
+            return self.query.get_count(using=self.db)
+        
