18812ee8be1b06bc051e7e6c6d15d345b6d64b73
chris <chris@coronis>
2015-03-24 17:04:48 -0400

    Added an admin list filter with a text field input.

--- admin_steroids/filters.py
+++ admin_steroids/filters.py
@@ -1,7 +1,7 @@
 import uuid
 
 from django.conf import settings
-from django.contrib.admin import FieldListFilter, SimpleListFilter
+from django.contrib.admin import FieldListFilter, SimpleListFilter, ListFilter
 from django.core.exceptions import ImproperlyConfigured, ValidationError
 from django.core.urlresolvers import reverse
 from django.core.cache import cache
@@ -273,3 +273,63 @@ class LogEntryAdminUserFilter(SimpleListFilter):
             return queryset.filter(user__pk=self.value())
 
         return queryset
+
+#http://stackoverflow.com/a/20588975/247542
+class SingleTextInputFilter(ListFilter):
+    """
+    renders filter form with text input and submit button
+    """
+    size = 40
+    parameter_name = None
+    template = 'admin_steroids/textinput_filter.html'
+    style = ''
+
+    def __init__(self, request, params, model, model_admin):
+        super(SingleTextInputFilter, self).__init__(
+            request, params, model, model_admin)
+        if self.parameter_name is None:
+            raise ImproperlyConfigured(
+                "The list filter '%s' does not specify "
+                "a 'parameter_name'." % self.__class__.__name__)
+
+        if self.parameter_name in params:
+            value = params.pop(self.parameter_name)
+            self.used_parameters[self.parameter_name] = value
+
+    def value(self):
+        """
+        Returns the value (in string format) provided in the request's
+        query string for this filter, if any. If the value wasn't provided then
+        returns None.
+        """
+        return self.used_parameters.get(self.parameter_name, None)
+
+    def has_output(self):
+        return True
+
+    def expected_parameters(self):
+        """
+        Returns the list of parameter names that are expected from the
+        request's query string and that will be used by this filter.
+        """
+        return [self.parameter_name]
+
+
+    def choices(self, cl):
+        all_choice = {
+            'selected': self.value() is None,
+            'query_string': cl.get_query_string({}, [self.parameter_name]),
+            'display': _('All'),
+        }
+        v = self.value()
+        return ({
+            'get_query': cl.params,
+            'current_value': '' if v is None else v,
+            'all_choice': all_choice,
+            'parameter_name': self.parameter_name,
+            'query_string': all_choice['query_string'],
+            'title': self.title,
+            'size': self.size,
+            'style': self.style,
+        }, )
+        
